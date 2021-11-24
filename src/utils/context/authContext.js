45dbcd00298f04b47e6b0ae71eed1cff37d250d5
Rodrigo <rodrigo1cabrera2@gmail.com>
2021-11-23 21:42:31 -0300

    https to http

--- src/utils/context/authContext.js
+++ src/utils/context/authContext.js
@@ -15,7 +15,7 @@ export function AuthProvider({ children }) {
 
 		axios({
 			method: "post",
-			url: "https://challenge-react.alkemy.org",
+			url: "http://challenge-react.alkemy.org",
 			data: {
 				email: email,
 				password: password,
